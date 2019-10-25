56a8cf6ed97906c3a2c352367db6aa7051ab68fb
Simone Campanoni <simo.xan@gmail.com>
2019-10-25 17:28:35 -0500

    Merge files

---
+++ src/CatPass.cpp
@@ -0,0 +1,49 @@
+#include "llvm/Pass.h"
+#include "llvm/IR/Function.h"
+#include "llvm/Support/raw_ostream.h"
+#include "llvm/IR/LegacyPassManager.h"
+#include "llvm/Transforms/IPO/PassManagerBuilder.h"
+
+using namespace llvm;
+
+namespace {
+  struct CAT : public FunctionPass {
+    static char ID; 
+
+    CAT() : FunctionPass(ID) {}
+
+    // This function is invoked once at the initialization phase of the compiler
+    // The LLVM IR of functions isn't ready at this point
+    bool doInitialization (Module &M) override {
+      errs() << "Hello LLVM World at \"doInitialization\"\n" ;
+      return false;
+    }
+
+    // This function is invoked once per function compiled
+    // The LLVM IR of the input functions is ready and it can be analyzed and/or transformed
+    bool runOnFunction (Function &F) override {
+      errs() << "Hello LLVM World at \"runOnFunction\"\n" ;
+      return false;
+    }
+
+    // We don't modify the program, so we preserve all analyses.
+    // The LLVM IR of functions isn't ready at this point
+    void getAnalysisUsage(AnalysisUsage &AU) const override {
+      errs() << "Hello LLVM World at \"getAnalysisUsage\"\n" ;
+      AU.setPreservesAll();
+    }
+  };
+}
+
+// Next there is code to register your pass to "opt"
+char CAT::ID = 0;
+static RegisterPass<CAT> X("CAT", "Homework for the CAT class");
+
+// Next there is code to register your pass to "clang"
+static CAT * _PassMaker = NULL;
+static RegisterStandardPasses _RegPass1(PassManagerBuilder::EP_OptimizerLast,
+    [](const PassManagerBuilder&, legacy::PassManagerBase& PM) {
+        if(!_PassMaker){ PM.add(_PassMaker = new CAT());}}); // ** for -Ox
+static RegisterStandardPasses _RegPass2(PassManagerBuilder::EP_EnabledOnOptLevel0,
+    [](const PassManagerBuilder&, legacy::PassManagerBase& PM) {
+        if(!_PassMaker){ PM.add(_PassMaker = new CAT()); }}); // ** for -O0
